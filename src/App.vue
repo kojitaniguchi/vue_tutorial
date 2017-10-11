0d90d63760a8aa357afd0ecb3e4d1ab07479f0b2
kojitaniguchi <tanigu0511@gmail.com>
2017-10-11 23:18:37 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+   <p v-if="msg.length > 0">
+    {{msg}}
+   <p v-else>
+    no text
+   </p>
+   <input type="text" v-model="msg">
+   <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  }
+}
+</script>
